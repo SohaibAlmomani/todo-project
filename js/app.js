5cfa55b793979b7401ec14c9e22221c87b366427
SohaibAlmomani <almomanisohaib95@yahoo.com>
2022-04-20 08:00:54 +0300

    todo-js2

--- js/app.js
+++ js/app.js
@@ -32,15 +32,127 @@ switch (welAlert) {
         break;
 }
 */
+//Task 6 :
 
-let Q1 =prompt("Are you male ?");
-let Q2 =prompt("Are you Jordanian ?");
-let Q3 =prompt("Are you looking for a job ?");
 
-switch(Q1,Q2,Q3){
-
-    case yes :
+let study =prompt("Are you an IT student ?").toLowerCase();
+if (study==="yes"){
+    console.log(study);
+    your_study();
+}else if(study==="no"){
+    console.log(study);
+}else {
+    study = "invalid";
+    console.log(study);
 }
+
+let nationality =prompt("Are you Jordanian ?").toLowerCase();
+if (nationality==="yes"){
+    console.log(nationality);
+    your_nationality(nationality) ;
+}else if (nationality==="no"){
+    console.log(nationality);
+}else{
+    nationality = "invalid";
+    console.log(nationality);
+}
+
+let job =prompt("Are you looking for a job ?").toLowerCase();
+if (job==="yes"){
+    console.log(job);
+    your_job();
+}else if (job==="no"){
+    console.log(job);
+}else{
+    job = "invalid";
+    console.log(job);
+}
+
+
+let arr1=[];
+arr1.push( study);
+arr1.push( nationality);
+arr1.push( job);
+
+
+for (let i=0 ; i < arr1.length ; i++ ){
+    console.log( (arr1[i]));
+    }
+
+    function your_study(study)
+    {
+        if (study="yes"){
+             alert("Good. lets see the next Question :) ");
+            }
+    }
+        
+    function your_nationality(nationality)
+    {
+            if (nationality="yes"){
+                 alert("Good. lets see the next Question :) ");
+                }
+    }
+        
+    function your_job (job)
+    {
+            if (job="yes"){
+                alert("Then Go To LTUC :)");
+            }
+    }
+
+
+/*
+
+function your_study(){
+
+let study =prompt("Are you an IT student ?").toLowerCase();
+if (study==="yes" || study==="no"){
+    console.log(study);
+}else{
+    study = "invalid";
+    console.log(study);
+}
+return 0;
+}
+
+let  your_nationality = () => {
+let nationality =prompt("Are you Jordanian ?").toLowerCase();
+if (nationality==="yes" || nationality==="no"){
+    console.log(nationality);
+}else{
+    nationality = "invalid";
+    console.log(nationality);
+}
+}
+
+let your_job = () => {
+let job =prompt("Are you looking for a job ?").toLowerCase();
+if (job==="yes" || job==="no"){
+    console.log(job);
+}else{
+    job = "invalid";
+    console.log(job);
+}
+}
+
+let study=your_study();
+let nationality=your_nationality();
+let job=your_job();
+
+
+let arr=[];
+arr.push( study);
+arr.push( nationality);
+arr.push( job);
+
+//console.log(your_nationality());
+for (let i=0 ; i < arr.length ; i++ ){
+console.log ( arr[i] );
+}
+
+*/
+
+
 
 
 
